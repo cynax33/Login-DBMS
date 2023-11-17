fd9344aab6603c373c9f9ff2496aebedbad6e295
Christian Luiz Malig-on <147081226+cynax33@users.noreply.github.com>
2023-11-17 23:30:59 +0800

    Add project files.

---
+++ Login/Login.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaceRecognition;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Login : Form
+    {
+        userdbDataContext db = new userdbDataContext();
+
+        FaceRec facerec = new FaceRec();
+
+        int instance;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result = db.sp_login(textBox1.Text, textBox2.Text).Count();
+
+            if(textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Fields must not be empty", "Warning");
+            }
+            else
+            {
+                if (result == 0)
+                {
+                    MessageBox.Show("Invalid username or password", "Log in");
+                }
+                else
+                {
+                    if (db.sp_type(textBox1.Text, textBox2.Text) == 0)
+                    {
+                        MessageBox.Show("Welcome Admin", "Log in");
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Welcome Staff", "Log in");
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (instance < 1)
+            {
+                facerec.openCamera(pictureBox1, pictureBox2);
+                instance += 1;
+
+                facerec.getPersonName(label3);
+
+                facerec.isTrained = true;
+            }
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Registration rform = new Registration();
+            this.Hide();
+            rform.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (label3 != null)
+            {
+                if (db.sp_faceLogin(label3.Text).Count() == 1)
+                {
+                    if(db.sp_typeFaceID(label3.Text) == 0)
+                    {
+                        MessageBox.Show("Welcome Admin", "Log in");
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Welcome Staff", "Log in");
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Face ID not found in database!", "Error");
+                }
+            }   
+        }
+    }
+}
---
+++ Login/Registration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+
+namespace Login
+{
+    public partial class Registration : Form
+    {
+        int instance;
+
+        userdbDataContext db = new userdbDataContext();
+
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        FaceRec facerec = new FaceRec();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(instance<1)
+            {
+                facerec.openCamera(pictureBox1, pictureBox2);
+                instance += 1;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            facerec.getPersonName(label4);
+            textBox1.Text = label4.Text;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (!isEmpty())
+            {
+                MessageBox.Show("Fields must not be empty!", "Warning");
+            }
+            else
+            {
+                string email = textBox5.Text;
+
+                db.sp_register(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text, textBox7.Text);
+                if(instance == 1)
+                {
+                    facerec.Save_IMAGE(email);
+                }
+                MessageBox.Show("Save Success");
+            }
+        }
+
+        private bool isEmpty()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox3.Text) || string.IsNullOrEmpty(textBox4.Text) || string.IsNullOrEmpty(textBox5.Text) || string.IsNullOrEmpty(textBox6.Text) || string.IsNullOrEmpty(textBox7.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }               
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            facerec.isTrained = true;
+        }
+    }
+}
